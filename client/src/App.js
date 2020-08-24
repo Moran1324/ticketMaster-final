b5d69fab558e129bebfd95cba5bba2d1aab6f6b7
Moran1324 <67854443+Moran1324@users.noreply.github.com>
2020-08-24 23:45:27 +0300

    Revert "auto load, ticket component"
    This reverts commit 3c9b1be5d7a4d2a3a441e7db915931dbc708e9ca.

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import Ticket from './components/Ticket';
-import axios from 'axios';
+// import logo from './logo.svg';
+// import Ticket from './components/Ticket'
 import './App.css';
-import { Button, TextField } from '@material-ui/core';
-
-
-
-
 
 function App() {
-
-  const [tickets, setTickets] = useState()
-
-  useEffect(() => {
-    const getTickets = async () => {
-      let { data } = await axios.get('/api/tickets');
-      setTickets(data);
-    }
-    getTickets();
-  }, [])
-
-
-  // onChange={filterTickets}
   return (
     <main>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      {tickets ?
-        <Ticket tickets={tickets} />
-        : null}
-      {/* <Button onClick={getTickets}>get tickets</Button> */}
+      <h1> this is test </h1>
     </main>
   );
 }
